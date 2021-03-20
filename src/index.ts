d19dcfef2b72bfee9322db64e7f19b7cd070eb98
Lucas Maciel <lucas.maciel@wisereducacao.com>
2021-03-20 18:27:45 -0300

    feat: add swagger documentation API

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import express from "express";
+import swageerUi from "swagger-ui-express";
 
 import { usersRoutes } from "./routes/users.routes";
+import swageerFile from "./swageer.json";
 
 const app = express();
 
 app.use(express.json());
+app.use("/api-docs", swageerUi.serve, swageerUi.setup(swageerFile));
 
 app.use("/users", usersRoutes);
 
